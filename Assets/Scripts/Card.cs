082bc62f4ea3ab82062d8e4d5f8d7eda7a2d2964
g00197439 <60353410+g00197439@users.noreply.github.com>
2020-10-21 16:03:51 -0400

    Bug with Decorator and CardDefinition classes not being accessiblefixed

--- Assets/Scripts/Card.cs
+++ Assets/Scripts/Card.cs
@@ -4,22 +4,24 @@ using UnityEngine;
 
 public class Card : MonoBehaviour
 {
-    [System.Serializable]
-    public class Decorator
-    {
-        // This class stores info about each decorator or pip from DeckXML
-        public string type; // For card pips, type = "pip"
-        public Vector3 loc; // The location of the Sprite on the Card
-        public bool flip = false; // Whether to flip the Sprite vertically
-        public float scale = 1f; // The scale of the Sprite
-    }
+    
+}
 
-    [System.Serializable]
-    public class CardDefinintion
-    {
-        // This class stores info for each rank of card
-        public string face; // Sprite to use for each face card
-        public int rank; // The rank (1-13) of this card
-        public List<Decorator> pips = new List<Decorator>(); // Pips used
-    }
+[System.Serializable]
+public class Decorator
+{
+    // This class stores info about each decorator or pip from DeckXML
+    public string type; // For card pips, type = "pip"
+    public Vector3 loc; // The location of the Sprite on the Card
+    public bool flip = false; // Whether to flip the Sprite vertically
+    public float scale = 1f; // The scale of the Sprite
+}
+
+[System.Serializable]
+public class CardDefinition
+{
+    // This class stores info for each rank of card
+    public string face; // Sprite to use for each face card
+    public int rank; // The rank (1-13) of this card
+    public List<Decorator> pips = new List<Decorator>(); // Pips used
 }
