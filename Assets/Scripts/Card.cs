bc9c88373feb02a55269fba4a119d821419752c1
g00197439 <60353410+g00197439@users.noreply.github.com>
2020-10-28 11:41:19 -0400

    Sorting layers created

--- Assets/Scripts/Card.cs
+++ Assets/Scripts/Card.cs
@@ -18,6 +18,59 @@ public class Card : MonoBehaviour
     public GameObject back; // The GameObject of the back of the card
     public CardDefinition def; // Parsed from DeckXML.xml
 
+    public SpriteRenderer[] spriteRenderers;
+
+    private void Start()
+    {
+        SetSortOrder(0); // Ensures that the card starts properly depth sorted
+    }
+
+    public void PopulateSpriteRenderers()
+    {
+        if(spriteRenderers == null || spriteRenderers.Length == 0)
+        {
+            spriteRenderers = GetComponentsInChildren<SpriteRenderer>();
+        }
+    }
+
+    public void SetSortingLayerName(string tName)
+    {
+        PopulateSpriteRenderers();
+
+        foreach(SpriteRenderer tSR in spriteRenderers)
+        {
+            tSR.sortingLayerName = tName;
+        }
+    }
+
+    public void SetSortOrder(int sOrd)
+    {
+        PopulateSpriteRenderers();
+
+        // Iterate through all the spriteRenderers as tSR
+        foreach (SpriteRenderer tSR in spriteRenderers)
+        {
+            if (tSR.gameObject == this.gameObject)
+            {
+                // If the gameObject is this.gameObject, it's the background
+                tSR.sortingOrder = sOrd; // Set it's order to sOrd
+                continue; // And continue to the next iteration of the loop
+            }
+            // Each of the children of this GameObject are name. Switch based on the names.
+            switch(tSR.gameObject.name)
+            {
+                case "back":
+                    tSR.sortingOrder = sOrd + 2;
+                    break;
+
+                case "face":
+                default:
+                    tSR.sortingOrder = sOrd + 1;
+                    break;
+            }
+        }
+    }
+
     public bool faceUp
     {
         get
--- Assets/Scripts/Prospector.cs
+++ Assets/Scripts/Prospector.cs
@@ -102,6 +102,8 @@ public class Prospector : MonoBehaviour
             cp.slotDef = tSD;
             // CardProspectors in the tableau have the state CardState.tableau
             cp.state = eCardState.tableau;
+            // CardProspectors in the tableau have the state CardState.tableau
+            cp.SetSortingLayerName(tSD.layerName); // Set the sorting layers
 
             tableau.Add(cp); // Add this CardProspector to the List<> tableau
         }
